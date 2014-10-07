9e09f14d54c84a2d3284352a03b13e9af9c6e396
Andrew Burton <abauckland@gmail.com>
2014-10-07 09:19:51 +0100

    seed references 071014

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,3 +1,3 @@
 include Sprig::Helpers
 
-sprig [Company, User, Tile, Background, Font, Detail, Design, Hour]
+sprig [Company, User, Mjweb::Tile, Mjweb::Background, Mjweb::Font, Mjweb::Detail, Mjweb::Design, Mjweb::Hour]
