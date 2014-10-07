dc222971ee67a09f1ba407ddbe573c888a07d72b
Andrew Burton <abauckland@gmail.com>
2014-10-07 09:26:19 +0100

    seed update 1 071014

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,3 +1,4 @@
 include Sprig::Helpers
 
-sprig [Company, User, Mjweb::Tile, Mjweb::Background, Mjweb::Font, Mjweb::Detail, Mjweb::Design, Mjweb::Hour]
+#sprig [Company, User, Mjweb::Tile, Mjweb::Background, Mjweb::Font, Mjweb::Detail, Mjweb::Design, Mjweb::Hour]
+sprig [Company,  Mjweb::Tile, Mjweb::Background, Mjweb::Font]
