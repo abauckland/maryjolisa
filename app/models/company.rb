982495e358191c941673fef54fd4981912d5d8a2
Rob Paskin <rjpaskin+git@gmail.com>
2014-10-23 14:57:21 +0100

    Add validations to subdomain and retail_subdomain on Company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,6 @@
 class Company < ActiveRecord::Base
+  SUBDOMAIN_REGEXP = /\A[a-z\-]+\z/
+
   has_many :users
   mount_uploader :logo, LogoUploader
 
@@ -8,12 +10,25 @@ class Company < ActiveRecord::Base
   geocoded_by :postcode
   after_validation :geocode, :if => :postcode_changed?
 
+  validates :subdomain, :retail_subdomain, presence: true, length: { maximum: 20 },
+    format: { with: SUBDOMAIN_REGEXP, message: "can only contain letters and dashes" }
+
+  validate :subdomain_uniqueness, :retail_subdomain_uniqueness
+
   def phone_number
     tel.unpack('A4A3A4').join(' ')
   end
 
   def www
     return subdomain+ '.' +domain
+  end
+
+  def subdomain=(text)
+    super(text.downcase)
+  end
+
+  def retail_subdomain=(text)
+    super(text.downcase)
   end
 
  def default_settings
@@ -26,5 +41,19 @@ class Company < ActiveRecord::Base
 
   def set_default_retail_subdomain
     self.retail_subdomain ||= "#{subdomain}-store"
+  end
+
+  def subdomain_uniqueness
+    return if subdomain.blank?
+    errors.add(:subdomain, :taken) if given_subdomain_exists?(subdomain)
+  end
+
+  def retail_subdomain_uniqueness
+    return if retail_subdomain.blank?
+    errors.add(:retail_subdomain, :taken) if given_subdomain_exists?(retail_subdomain)
+  end
+
+  def given_subdomain_exists?(domain)
+    Company.where("subdomain = :domain OR retail_subdomain = :domain", domain: domain).any?
   end
 end
--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -12,4 +12,62 @@ class CompanyTest < ActiveSupport::TestCase
 
     assert_equal "cookieshq-shop", company.retail_subdomain
   end
+
+  [:subdomain, :retail_subdomain].each do |field|
+    test "allows letters and dashes in #{field}" do
+      company = Company.new(field => "cookies-hq")
+
+      company.valid?
+
+      assert company.errors[field].empty?
+    end
+
+    test "allows uppercase letters in #{field} (but downcases them)" do
+      company = Company.new(field => "COOKIES-hq")
+
+      company.valid?
+
+      assert company.errors[field].empty?
+    end
+
+    test "disallows numbers in #{field}" do
+      company = Company.new(field => "cookies1")
+
+      company.valid?
+
+      assert_equal company.errors[field], ["can only contain letters and dashes"]
+    end
+  end
+
+  test "subdomain must be unique" do
+    new_company = Company.new(subdomain: companies(:one).subdomain)
+
+    new_company.valid?
+
+    assert new_company.errors[:subdomain].include?(I18n.t "errors.messages.taken")
+  end
+
+  test "subdomain must be unique (including retail_subdomain)" do
+    new_company = Company.new(subdomain: companies(:one).retail_subdomain)
+
+    new_company.valid?
+
+    assert new_company.errors[:subdomain].include?(I18n.t "errors.messages.taken")
+  end
+
+  test "retail_subdomain must be unique" do
+    new_company = Company.new(retail_subdomain: companies(:one).retail_subdomain)
+
+    new_company.valid?
+
+    assert new_company.errors[:retail_subdomain].include?(I18n.t "errors.messages.taken")
+  end
+
+  test "retail_subdomain must be unique (including subdomain)" do
+    new_company = Company.new(retail_subdomain: companies(:one).subdomain)
+
+    new_company.valid?
+
+    assert new_company.errors[:retail_subdomain].include?(I18n.t "errors.messages.taken")
+  end
 end
