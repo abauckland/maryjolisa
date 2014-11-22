183d3aee69646527535966643960a28afae076bd
Andrew Burton <abauckland@gmail.com>
2014-11-22 19:17:17 +0000

    error noticed added to destroy action

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -58,7 +58,7 @@ class CompaniesController < ApplicationController
   def destroy
     @company.destroy
     respond_to do |format|
-      format.html { redirect_to companies_url }
+      format.html { redirect_to companies_url, notice: 'Company was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
