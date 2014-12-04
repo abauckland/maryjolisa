2ad6009737301dd75fd5d88f662d450e78fae186
Andrew Burton <abauckland@gmail.com>
2014-12-04 13:34:10 +0000

    remove bugsnag

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,8 @@ set :default_stage, "production"
 
 set :rvm_type, :user
 
-require "bugsnag/capistrano"
-set :bugsnag_api_key, "4749e41c02e44cf6285fe8ea70c1cf46"
+#require "bugsnag/capistrano"
+#set :bugsnag_api_key, "4749e41c02e44cf6285fe8ea70c1cf46"
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
