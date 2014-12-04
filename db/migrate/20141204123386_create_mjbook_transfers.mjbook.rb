69a70a66a5951f1ed00b154a48998218879c13d5
Andrew Burton <abauckland@gmail.com>
2014-12-04 12:55:26 +0000

    type correction

--- db/migrate/20141204123386_create_mjbook_transfers.mjbook.rb
+++ db/migrate/20141204123386_create_mjbook_transfers.mjbook.rb
@@ -4,7 +4,7 @@ class CreateMjbookTransfers < ActiveRecord::Migration
     create_table :mjbook_transfers do |t|
       t.integer :company_id
       t.integer :user_id
-      t.ingeter :account_from_id
+      t.integer :account_from_id
       t.integer :account_to_id
       t.integer :paymethod_id
       t.string :total
