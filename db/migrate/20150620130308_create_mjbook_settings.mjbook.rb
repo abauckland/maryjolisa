673941828fde2e25bcebb3aee1d9e80ac846d1c9
Andrew Burton <abauckland@gmail.com>
2015-01-20 19:44:33 +0000

    settings migration

---
+++ db/migrate/20150620130308_create_mjbook_settings.mjbook.rb
@@ -0,0 +1,14 @@
+# This migration comes from mjbook (originally 20150120130308)
+class CreateMjbookSettings < ActiveRecord::Migration
+  def change
+    create_table :mjbook_settings do |t|
+      t.integer :company_id
+      t.string :email_domain
+      t.string :email_username
+      t.string :email_password
+      t.timestamp :yearend
+
+      t.timestamps
+    end
+  end
+end
