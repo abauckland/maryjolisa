e390eb09bd9af41ff22355515ae417ebc1530739
Andrew Burton <abauckland@gmail.com>
2015-04-23 09:38:06 +0100

    correct class name

--- db/migrate/20152304090752_change_company_plan.rb
+++ db/migrate/20152304090752_change_company_plan.rb
@@ -1,4 +1,4 @@
-class AddCompanyPlan < ActiveRecord::Migration
+class ChangeCompanyPlan < ActiveRecord::Migration
   def change
     remove_column :companies, :plan
     add_column :companies, :group, :integer
