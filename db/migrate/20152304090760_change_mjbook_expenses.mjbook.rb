0c4b31da1dcfbf919d1a493c3c1da4c40a4f08dc
Andrew Burton <abauckland@gmail.com>
2015-06-26 14:47:10 +0100

    update mjbook migration

---
+++ db/migrate/20152304090760_change_mjbook_expenses.mjbook.rb
@@ -0,0 +1,10 @@
+# This migration comes from mjbook (originally 20150626142432)
+class ChangeMjbookExpenses < ActiveRecord::Migration
+  def change
+    remove_column :mjbook_expenses, :mileage_id
+    remove_column :mjbook_expenses, :expend_id
+
+    add_column :mjbook_expenses, :notes, :text
+
+  end
+end
