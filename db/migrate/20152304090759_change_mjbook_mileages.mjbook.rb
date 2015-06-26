d161ff9c4d83da06b07087f1f1d32e0b330f2492
Andrew Burton <abauckland@gmail.com>
2015-06-26 09:36:50 +0100

    update mjbook inc change migration

---
+++ db/migrate/20152304090759_change_mjbook_mileages.mjbook.rb
@@ -0,0 +1,9 @@
+# This migration comes from mjbook (originally 20150624161512)
+class ChangeMjbookCompanyaccounts < ActiveRecord::Migration
+  def change
+    remove_column :mjbook_mileages, :date
+    remove_column :mjbook_mileages, :hmrcexpcat_id
+
+    add_column :mjbook_mileages, :travel_date, :timestamp
+  end
+end
